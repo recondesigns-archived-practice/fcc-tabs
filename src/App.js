247c5b2ecbd10a7983a754a3c32c46aa101ae257
The Stedman <46408716+recondesigns@users.noreply.github.com>
2021-07-04 23:31:08 -0500

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="App">
       <AppContainer>
-        <h1>{"App Component"}</h1>
+        <h1>{"Tabs Project"}</h1>
       </AppContainer>
     </div>
   );
