17c17850e9d6a2fc2d4457fb05a2967e024fc55b
The Stedman <46408716+recondesigns@users.noreply.github.com>
2021-07-05 00:02:18 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  padding: 80px 20px 0px 20px;
+  background: rgb(241, 245, 248);
   border: 1px solid lightcoral;
 `;
 
 const Title = styled.h1`
+  margin-bottom: 8px;
   font-family: "Roboto", sans-serif;
+  font-size: 40px;
+  line-height: 40px;
   text-align: center;
+  color: #414141;
   border: 1px dashed green;
 `;
 
 const Subtitle = styled.h2`
+  font-size: 16px;
+  line-height: 24px;
   font-family: "Open Sans", sans-serif;
   text-align: center;
+  color: #414141;
+  opacity: 0.8;
   border: 1px dashed green;
 `;
 
